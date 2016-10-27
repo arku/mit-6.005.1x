8db5cd274507ed1e0d24099c11351231265670f1
Arun Kumar <arunkumarm1995@gmail.com>
2016-10-27 23:33:21 +0530

    Add tests for Extract class methods

--- ps2/test/twitter/ExtractTest.java
+++ ps2/test/twitter/ExtractTest.java
@@ -3,7 +3,11 @@ package twitter;
 import static org.junit.Assert.*;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
@@ -11,8 +15,26 @@ import org.junit.Test;
 public class ExtractTest {
 
     /*
-     * TODO: your testing strategies for these methods should go here.
-     * Make sure you have partitions.
+     * TODO: Testing strategy:
+     * Partitioning based on Interval
+     * 1. Same timestamps
+     * 2. Different timestamps
+     * 
+     * Partitioning based on quantity of tweets:
+     * 1. Empty set of tweets
+     * 2. Single tweet
+     * 3. More than one tweet
+     * 
+     * Partitioning based on mentions
+     * 1. No mentions
+     * 2. Single mention
+     * 3. More than a mention
+     * 4. Invalid mention ie. a mention preceded or followed by one of the following
+     *    characters A-Z, a-z, 0-9, _, -
+     * Partitioning based on case
+     * 1. lowercase mention
+     * 2. uppercase mention
+     * 3. togglecase mention 
      */
     
     private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
@@ -35,10 +57,77 @@ public class ExtractTest {
     }
     
     @Test
+    public void testGetTimespanNoTweets() {
+        Instant minInstant = Instant.MIN, maxInstant = Instant.MAX;
+        Timespan timespan = Extract.getTimespan(Arrays.asList());
+        
+        assertEquals("expected start", minInstant, timespan.getStart());
+        assertEquals("expected end", maxInstant, timespan.getEnd());
+    }
+    
+    @Test
+    public void testGetTimeSpanMoreThanOneTweet() {
+        Instant latest = Instant.parse("2016-03-10T19:00:00Z"), oldest = Instant.parse("2016-01-10T03:00:00Z");
+        Tweet latestTweet = new Tweet(345, "Anonymous", "Nothing fancy", latest);
+        Tweet oldestTweet = new Tweet(32, "Anonymous", "Feeling crazy", oldest);
+        
+        Timespan timespan = Extract.getTimespan(Arrays.asList( latestTweet, tweet1, tweet2, oldestTweet));
+        
+        assertEquals("expected start", oldest, timespan.getStart());
+        assertEquals("expected start", latest, timespan.getEnd());
+    }
+    
+    public void testGetTimeSpanSameTimestamp() {
+        Instant instantOne = Instant.parse("2016-03-10T19:00:00Z");
+        Tweet tweetOne = new Tweet(90, "Anonymous", "Nothing fancy", instantOne);
+        Tweet tweetTwo = new Tweet(83, "Anonymous", "Feeling crazy", instantOne);
+        
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweetOne, tweetTwo));
+        
+        assertEquals("expected start", instantOne, timespan.getStart());
+        assertEquals("expected start", instantOne, timespan.getEnd());
+    }
+    
+    @Test
     public void testGetMentionedUsersNoMention() {
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
         
         assertTrue("expected empty set", mentionedUsers.isEmpty());
+    }
+    
+    @Test
+    public void testGetMentionedUsersNoTweets() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList());
+        assertTrue("no mentioned users", mentionedUsers.isEmpty());
+    }
+    
+    @Test
+    public void testGetMentionedUsersSingleMention() {
+        Tweet singleMentionTweet = new Tweet(3, "Joseph", "It was a great event @JOHN hosted by @john", Instant.now());
+        
+        Set<String> actualMentions = Extract.getMentionedUsers(Arrays.asList(singleMentionTweet));
+        Set<String> expectedMentions = new HashSet<String>();
+        expectedMentions.add("john");
+        assertEquals(expectedMentions,actualMentions);
+    }
+    
+    @Test
+    public void testGetMentionedUsersMoreThanMention() {
+        Tweet moreThanMentionTweet = new Tweet(3, "Joseph", "@JOhn It was a great event hosted by @john, thanks to @rick for organizing", Instant.now());
+        
+        Set<String> actualMentions = Extract.getMentionedUsers(Arrays.asList(moreThanMentionTweet));
+        Set<String> expectedMentions = new HashSet<String>();
+        expectedMentions.add("john");
+        expectedMentions.add("rick");
+        assertEquals(expectedMentions,actualMentions);
+    }
+    
+    @Test
+    public void testGetMentionedUsersInvalidMention() {
+        Tweet singleMentionTweet = new Tweet(3, "Joseph", "It was a great event hosted by i@john", Instant.now());
+        
+        Set<String> actualMentions = Extract.getMentionedUsers(Arrays.asList(singleMentionTweet));
+        assertTrue("expected no mentions", actualMentions.isEmpty());
     }
 
     /*
