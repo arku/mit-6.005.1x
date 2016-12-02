61afb642cc49f8db7939c4aed6aa4d3c725c9112
Arun Kumar <arunkumarm1995@gmail.com>
2016-12-02 11:36:58 +0530

    Document and add junit tests for BookCopy

--- ps3/test/library/BookCopyTest.java
+++ ps3/test/library/BookCopyTest.java
@@ -13,17 +13,88 @@ public class BookCopyTest {
     /*
      * Testing strategy
      * ==================
+     * BookCopy():
+     *  exception
+     * getBook():
+     *  call getBook() and observe the returned book
+     * getCondition():
+     *  good, damaged
+     * toString():
+     *  statuses of good, damaged
+     * equals():
+     *  check for referential equality
+     *  reflexivity, symmetry and transitivity
      * 
-     * TODO: your testing strategy for this ADT should go here.
-     * Make sure you have partitions.
      */
     
-    // TODO: put JUnit @Test methods here that you developed from your testing strategy
     @Test
-    public void testExampleTest() {
-        Book book = new Book("This Test Is Just An Example", Arrays.asList("You Should", "Replace It", "With Your Own Tests"), 1990);
+    public void testBookCopyConstructor() throws Exception {
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
         BookCopy copy = new BookCopy(book);
         assertEquals(book, copy.getBook());
+    }
+    
+    @Test(expected=Exception.class)
+    public void testBookCopyConstructorException() throws Exception {
+        BookCopy copy = new BookCopy(null);
+    }
+    
+    
+    @Test
+    public void testGetBook() throws Exception {
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
+        BookCopy copy = new BookCopy(book);
+        assertEquals(book, copy.getBook());
+    }
+    
+    @Test
+    public void testGetGoodCondition() throws Exception {
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
+        BookCopy copy = new BookCopy(book);
+        assertEquals(BookCopy.Condition.GOOD, copy.getCondition());
+    }
+    
+    @Test
+    public void testGetDamagedCondition() throws Exception {
+        BookCopy.Condition damaged = BookCopy.Condition.DAMAGED;
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
+        BookCopy copy = new BookCopy(book);
+        
+        copy.setCondition(damaged);
+        assertEquals(damaged, copy.getCondition());
+    }
+    
+    @Test
+    public void testToStringGoodCondition() throws Exception {
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
+        BookCopy copy = new BookCopy(book);
+        assertTrue(copy.toString().indexOf("good") >= 0);
+    }
+    
+    @Test
+    public void testToStringDamagedCondition() throws Exception {
+        BookCopy.Condition damaged = BookCopy.Condition.DAMAGED;
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
+        BookCopy copy = new BookCopy(book);
+        copy.setCondition(damaged);
+        assertTrue(copy.toString().indexOf("damaged") >= 0);
+    }
+    
+    @Test
+    public void testEquals() throws Exception {
+        Book book = new Book("Not good", Arrays.asList("Jon", "Joe"), 1990);
+        Book anotherBook = book;
+        Book yetAnotherBook = anotherBook;
+        Book oneMoreBook = new Book("Okay", Arrays.asList("Jon") , 2000);
+        
+        assertTrue(book.equals(book));
+
+        assertTrue(book.equals(anotherBook));
+        assertTrue(anotherBook.equals(book));
+
+        assertTrue(anotherBook.equals(yetAnotherBook));
+        assertTrue(book.equals(yetAnotherBook));
+        assertFalse(book.equals(oneMoreBook));
     }
     
     @Test(expected=AssertionError.class)
